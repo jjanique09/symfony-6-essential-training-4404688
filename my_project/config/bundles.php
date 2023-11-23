f8e8e7c6b31cf3f765450e342fe3d76213d81e09
jjanique09 <71652541+jjanique09@users.noreply.github.com>
2023-11-23 11:25:58 +0100

    shopware bundle install

--- my_project/config/bundles.php
+++ my_project/config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Shopware\AppBundle\ShopwareAppBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
